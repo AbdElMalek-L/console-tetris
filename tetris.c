86d56c9911059d0d3f99fa52b35c36697e487429
abdelmalek-lamkadem <abdelmalek.lamkadem.23@ump.ac.ma>
2025-01-17 22:03:37 +0100

    add the tetris logo and colors

--- tetris.c
+++ tetris.c
@@ -8,8 +8,15 @@
 
 #define SCREEN_WIDTH 500
 #define SCREEN_HEIGHT 700
-#define BOARD_WIDTH 20 
-#define BOARD_HEIGHT 30
+#define BOARD_HEIGHT 40 
+#define BOARD_WIDTH 60 // normalment hna khasha tkon 30 walakin the 178 character am working with is too thin.{}
+
+#define COLOR_RED     12
+#define COLOR_GREEN   10
+#define COLOR_YELLOW  14
+#define COLOR_BLUE    9
+#define COLOR_DEFAULT 7 // Default console color
+
 
 const int TETROMINO_I[4] = {
     1,1,1,1
@@ -29,15 +36,32 @@ const int TETROMINO_O[2][2] = {
 
    //TODO: the rest of TETROMINOS
 
+const short TETRIS_LOGO[5][23] = {
+    {1,1,1,0,2,2,2,0,1,1,1,0,3,3,0,0,4,4,4,0,2,2,2},
+    {0,1,0,0,2,0,0,0,0,1,0,0,3,0,3,0,0,4,0,0,2,0,0},
+    {0,1,0,0,2,2,0,0,0,1,0,0,3,3,0,0,0,4,0,0,2,2,2},
+    {0,1,0,0,2,0,0,0,0,1,0,0,3,0,3,0,0,4,0,0,0,0,2},
+    {0,1,0,0,2,2,2,0,0,1,0,0,3,0,3,0,4,4,4,0,2,2,2}
+};
+
 void setConsoleSize(int width, int height);
 void goToXY(short x, short y);
 void drawWelcomePage(int width, int height);
+void waitForNextFrame(short hz);
+void setTextColor(short color);
+
+boolean game_exit = 0;
 
 
 int main() {
     setConsoleSize(SCREEN_WIDTH,SCREEN_HEIGHT);
+    
+    while(!game_exit){
+        drawWelcomePage(BOARD_WIDTH,BOARD_HEIGHT);
+        waitForNextFrame(10); // Hz
+	}
 
-    drawWelcomePage(BOARD_WIDTH,BOARD_HEIGHT);
+
 
     return 0;
 }
@@ -57,6 +81,11 @@ void setConsoleSize(int width, int height) {
 
 }
 
+void setTextColor(short color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
 void goToXY(short x, short y) {
     HANDLE hConsoleOutput;
     COORD Cursor_Pos = {x,y};
@@ -64,16 +93,61 @@ void goToXY(short x, short y) {
     hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleCursorPosition(hConsoleOutput, Cursor_Pos);
 }
+void waitForNextFrame(short hz){
+    int milli_seconds = 1 / hz * 1000;
+    clock_t start_time = clock();
+    while(clock() < start_time + milli_seconds);
+}
 
-void drawWelcomePage(int width, int height) {
+void drawWelcomePage(int height, int width) {
     char borders = 178;
-    for(int y = 0; y < width; y++){
-		for(int x = 0; x < height; x++){
+    for(int x = 0; x < height; x++){
+		for(int y = 0; y <width ; y++){
 			if( (x == 0) || (y == 0) || (y == width-1) || (x == height-1)){
-				goToXY(x,y);
+				//DONE: Borders 
+                goToXY(x,y);
 				printf("%c%c",borders, borders);
 			}
 			
 		}
 	}
+    for(int x = 0; x < 5; x++){
+		for(int y = 0; y < 23; y++){
+			if(0 != TETRIS_LOGO[x][y]){
+
+                switch (TETRIS_LOGO[x][y]) {
+                    case 1: setTextColor(COLOR_RED); break;
+                    case 2: setTextColor(COLOR_GREEN); break;
+                    case 3: setTextColor(COLOR_YELLOW); break;
+                    case 4: setTextColor(COLOR_BLUE); break;
+                }
+                
+                goToXY(y*2+8,x+3);
+				printf("%c%c" ,borders, borders);
+                setTextColor(COLOR_DEFAULT); // Reset to default color
+				//TODO: tetris logo
+
+                /*
+                
+                ▓▓▓▓▓▓  ▓▓▓▓▓▓  ▓▓▓▓▓▓  ▓▓▓▓    ▓▓▓▓▓▓  ▓▓▓▓▓▓
+                  ▓▓    ▓▓        ▓▓    ▓▓  ▓▓    ▓▓    ▓▓      
+                  ▓▓    ▓▓▓▓      ▓▓    ▓▓▓▓      ▓▓    ▓▓▓▓▓▓  
+                  ▓▓    ▓▓        ▓▓    ▓▓  ▓▓    ▓▓        ▓▓  
+                  ▓▓    ▓▓▓▓▓▓    ▓▓    ▓▓  ▓▓  ▓▓▓▓▓▓  ▓▓▓▓▓▓  
+
+                1111110011111100111111001111000011111100111111
+                0011000011000000001100001100110000110000110000
+                0011000011110000001100001111000000110000111111
+                0011000011000000001100001100110000110000000011
+                0011000011111100001100001100110000110000111111
+                
+                
+                */
+                
+
+            }
+        }
+    }
 }
+
+
